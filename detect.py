28210d0b4b91a3586866bf3c57ea391196b23e2b
thanley4 <93823322+thanley4@users.noreply.github.com>
2022-01-21 10:43:40 +0000

    Update detect.py
    Basic width detection along with colour classifying for people, cars and cyclists

--- detect.py
+++ detect.py
@@ -44,8 +44,6 @@ total_sensor_width_pixels = 3280  # pixels
 sensor_focal_length_mm = 3.04  # mm
 sensor_field_of_view_degrees = 62.2  # degrees
 
-objects_in_frame = []
-
 object_names = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
                 "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
                 "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
@@ -56,15 +54,15 @@ object_names = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "tr
                 "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
                 "scissors", "teddy bear", "hair drier", "toothbrush"]
 object_heights = [1.72, 1.05, 1.6]
+object_colours = ["red", "orange", "yellow"]
+
+objects_in_frame = []
 
 
 def object_distance(object_id, object_height_pixels):
-    if object_id < 3:
-        object_height_on_sensor_mm = total_sensor_height_mm * object_height_pixels / total_sensor_height_pixels
-        distance_to_object = round(object_heights[object_id] * sensor_focal_length_mm / object_height_on_sensor_mm, 3)
-        print(object_names[object_id], " is ", distance_to_object, "meters away")
-    else:
-        distance_to_object = 100
+    object_height_on_sensor_mm = total_sensor_height_mm * object_height_pixels / total_sensor_height_pixels
+    distance_to_object = round(object_heights[object_id] * sensor_focal_length_mm / object_height_on_sensor_mm, 3)
+    print(object_names[object_id], " is ", distance_to_object, "meters away")
     return distance_to_object
 
 
@@ -74,20 +72,41 @@ def object_angle(object_id, x_location):
     return angle_to_object
 
 
+def object_width(object_id, object_height_pixels, object_width_pixels):
+    object_width_m = round(object_width_pixels * object_heights[object_id] / object_height_pixels, 3)
+    return object_width_m
+
+
 class Object:
-    def __init__(self, distance, angle):
-        self.distance = distance
-        self.angle = angle
+    def __init__(self, object_id, object_height_pixels, object_width_pixels, object_x_location):
+        self.object_id = object_id
+        self.distance = object_distance(object_id, object_height_pixels)
+        self.angle = object_angle(object_id, object_x_location)
+        self.width = object_width(object_id, object_height_pixels, object_width_pixels)
 
 
 def drawObjects(objects, image_path, output_path):
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
     colors = ["red", "green", "blue", "yellow", "purple", "orange"]
+
+    points = [(417, 0), (960, 900), (1503, 0)]
+    draw.line(points, width=5, fill="gray", joint="curve")
+    draw.arc((860, 800, 1060, 1000), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((760, 700, 1160, 1100), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((660, 600, 1260, 1200), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((560, 500, 1360, 1300), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((460, 400, 1460, 1400), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((360, 300, 1560, 1500), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((260, 200, 1660, 1600), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((160, 100, 1760, 1700), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc(( 60, 000, 1860, 1800), start=-121.1, end=-58.9, width=1, fill="gray")
+    draw.arc((-40, -100, 1960, 1900), start=-121.1, end=-58.9, width=1, fill="gray")
+
     for i in objects:
         x = 960 + i.distance * 10 * math.sin(math.radians(i.angle))
         y = 900 - i.distance * 10 * math.cos(math.radians(i.angle))
-        draw.rectangle((x - 50, y - 50, x + 50, y + 50), fill = random.choice(colors))
+        draw.ellipse((x - (10 * i.width / 2), y - (10 * i.width / 2), x + (10 * i.width / 2), y + (10 * i.width / 2)), fill=object_colours[i.object_id])
     image.save(output_path)
     image.show(image)
 
@@ -294,12 +313,15 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
-                        height_in_pixels = int(xyxy[3] - xyxy[1])
-                        x_location = float(((xyxy[2] - xyxy[0]) / 2) + xyxy[0])
-                        distance = object_distance(c, height_in_pixels)
-                        angle = object_angle(c, x_location)
-                        objects_in_frame.append(Object(distance, angle))
-                        label = None if hide_labels else (names[c] if hide_conf else f'{distance} {angle} {names[c]} {conf:.2f}')
+                        if c < 3:
+                            height_in_pixels = int(xyxy[3] - xyxy[1])
+                            width_in_pixels = int(xyxy[2] - xyxy[0])
+                            x_location = float(((xyxy[2] - xyxy[0]) / 2) + xyxy[0])
+                            object_detected = Object(c, height_in_pixels, width_in_pixels, x_location)
+                            objects_in_frame.append(object_detected)
+                            label = None if hide_labels else (names[c] if hide_conf else f'{object_detected.width} {object_detected.distance} {object_detected.angle} {names[c]} {conf:.2f}')
+                        else:
+                            label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         annotator.box_label(xyxy, label, color=colors(c, True))
                         if save_crop:
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
